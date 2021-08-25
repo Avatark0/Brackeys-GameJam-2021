d730b0f04a717cb9ab5697e09b932045db24a67b
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2021-08-25 17:36:31 -0300

    Cleaning Project folder
    Project folder started as a clone of an old project. Cleaning most of the files that won't be used.

--- MyDearFox/Assets/_Scripts/Grimorio/ListaDeHabilidades.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using Grimorio;
-
-public class ListaDeHabilidades : MonoBehaviour
-{
-    [SerializeField] public List<HabilidadeSO> habilidades;
-}
--- MyDearFox/Assets/_Scripts/_Legacy/Deck.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-//[CreateAssetMenu(fileName="Deck", menuName="Deck",order=1)]
-public class Deck : ScriptableObject
-{
-    [SerializeField] private int[] cores;
-    [SerializeField] private FichaCriatura[] Criaturas;
-}
