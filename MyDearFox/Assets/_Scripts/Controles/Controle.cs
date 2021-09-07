67a4a2121a388e63bab12d7a0c01d5b79476738b
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2021-09-07 13:33:01 -0300

    Importing assets
    Importing assets from older version. Script jogador got compromised in the process. Turns out i gives no warns whatsoever when overriding a script with the same name as the import. Oh boi...

---
+++ MyDearFox/Assets/_Scripts/Controles/Controle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Controle : MonoBehaviour
+{
+    public bool isPlayer;
+    protected Collider2D otherCollider;
+    protected bool grounded;
+    protected float time;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Possuivel"){
+            Debug.Log(gameObject.name+": enter trigger, other = "+other);
+            otherCollider=other;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject.tag == "Possuivel"){
+            Debug.Log(gameObject.name+": exit trigger, other = "+other);
+            otherCollider=null;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Chao"){
+            grounded=true;
+            Debug.Log(gameObject.name+": landed");
+        }
+    }
+
+    private void Start(){
+        ResetValues();
+    }
+
+    protected abstract void ResetValues();
+
+}
---
+++ MyDearFox/Assets/_Scripts/Controles/ControleLebre.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleLebre : Controle
+{
+    [SerializeField] private float mass = default;//2f
+    [SerializeField] private float speed = default;
+    [SerializeField] private float jumpSpeed = default;
+    [SerializeField] private float jumpBufferTime = default;
+    
+    [SerializeField] private Rigidbody2D rigbd = default;
+    
+    public void Update()
+    {
+        Vector3 pos = transform.position;
+            
+        if(isPlayer){
+            if(Input.GetKey(KeyCode.A)){
+                pos.x-=speed*Time.deltaTime;
+                //Debug.Log("left");
+            }
+            
+            if(Input.GetKey(KeyCode.D)){
+                pos.x+=speed*Time.deltaTime;
+                //Debug.Log("right");
+            }
+
+            if(Input.GetKey(KeyCode.Space)){
+                Jump();
+            }
+            else{
+                rigbd.gravityScale=1;
+                //Debug.Log("released jump at "+time);
+            }
+        }
+
+        transform.position = pos;
+    }
+
+    private void Jump(){
+        if(grounded){
+            rigbd.velocity = Vector2.zero;
+            rigbd.AddForce(new Vector2(0,jumpSpeed));
+            grounded=false;
+            time = Time.time;
+            Debug.Log("jumped");
+        }
+        else if(time+jumpBufferTime >= Time.time){
+            rigbd.gravityScale=0;
+            //Debug.Log("buffering jump, gravityScale = "+rigbd.gravityScale);
+        }
+        else{
+            rigbd.gravityScale=1;
+            Debug.Log("released jump");
+        }
+    }
+
+    protected override void ResetValues(){
+        grounded=true;
+        rigbd.mass=mass;
+    }
+}
